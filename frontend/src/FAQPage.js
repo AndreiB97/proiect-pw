90460645bb5d4744688213a8a1e744ea9b372b59
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-21 10:21:41 +0300

    finished faq page

--- frontend/src/FAQPage.js
+++ frontend/src/FAQPage.js
@@ -1,18 +1,53 @@
-import React, {useState} from "react";
-import './FAQPage.scss'
+import React from "react";
+import './FAQPage.scss';
 import CollapsibleItem from "./CollapsibleItem";
+import axios from 'axios';
 
-// TODO get FAQ from server and display
+class FAQPage extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.getFAQ = this.getFAQ.bind(this);
 
-function FAQPage() {
+        this.state = {
+            'faq': []
+        };
+    }
 
-    return (
-        <div className={'FAQPage'}>
-            <h1>Frequently Asked Questions</h1>
-            <CollapsibleItem header={'test'} content={'lorem ipsum'}/>
-        </div>
-    )
+    async getFAQ() {
+        const result = await axios.get(
+            'http://localhost:80/faq'
+        );
+
+        this.state.faq = result.data.faq;
+        this.forceUpdate();
+    }
+
+    async componentDidMount() {
+        console.log('here');
+        console.log(this.state);
+        await this.getFAQ();
+        console.log(this.state);
+    }
+
+    render() {
+        /*
+
+         */
+        return (
+            <div className={'FAQPage'}>
+                <h1>Frequently Asked Questions</h1>
+                {
+                    this.state.faq.map((item) => {
+                        return (
+                            <CollapsibleItem header={item.Message.substr(0, 32)}
+                                             content={item.Message} response={item.Response} />
+                            );
+                    })
+                }
+            </div>
+        );
+    }
 }
 
 export default FAQPage;
