c02acdf7909b52e42b758cfdb6cc4d909c2d5d31
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-21 11:36:04 +0300

    added question display

--- frontend/src/VotePage.js
+++ frontend/src/VotePage.js
@@ -1,22 +1,55 @@
 import React from "react";
 import './VotePage.scss';
+import axios from "axios";
 
 // TODO connect to backend and get questions
 // TODO submit question (after login)
 // TODO score question
 
-function VotePage() {
-    return (
-        <div className={'VotePage'}>
-            <div className={'ButtonsContainer'}>
-                <button className={'NavigationButton'}>{'<'}</button>
-                <button className={'ChoiceButton'} id={'blue_button'}>Blue</button>
-                <div id={'or'}>or</div>
-                <button className={'ChoiceButton'} id={'red_button'}>Red</button>
-                <button className={'NavigationButton'}>{'>'}</button>
+class VotePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    async componentDidMount() {
+        const result = await axios.get(
+            'http://localhost:80/questions'
+        )
+
+        this.state.current_question_index = 0;
+        this.state.questions = result.data.question;
+
+        console.log(this.state);
+        this.forceUpdate();
+    }
+
+    render() {
+        return (
+            <div className={'VotePage'}>
+                <div className={'ButtonsContainer'}>
+                    <button className={'NavigationButton'}>{'<'}</button>
+                    <button className={'ChoiceButton'} id={'blue_button'}>
+                        {
+                            'questions' in this.state ?
+                                this.state.questions[this.state.current_question_index].Answer1 :
+                                'Blue'
+                        }
+                    </button>
+                    <div id={'or'}>or</div>
+                    <button className={'ChoiceButton'} id={'red_button'}>
+                        {
+                            'questions' in this.state ?
+                                this.state.questions[this.state.current_question_index].Answer2 :
+                                'Red'
+                        }
+                    </button>
+                    <button className={'NavigationButton'}>{'>'}</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default VotePage;
