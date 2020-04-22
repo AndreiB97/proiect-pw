ddabb9754fce64944d008422e10cf6909aebb53d
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-22 15:11:29 +0300

    added answer selection

--- frontend/src/VotePage.js
+++ frontend/src/VotePage.js
@@ -2,15 +2,44 @@ import React from "react";
 import './VotePage.scss';
 import axios from "axios";
 
-// TODO connect to backend and get questions
 // TODO submit question (after login)
 // TODO score question
+// TODO buttons get pressed when typing? (seems to be related to app state change)
+//  maybe add a flag in app state and only get question when flag is false
 
 class VotePage extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {};
+
+        this.get_question = this.get_question.bind(this);
+        this.onPrevClick = this.onPrevClick.bind(this);
+        this.onNextClick = this.onNextClick.bind(this);
+        this.pick_answer = this.pick_answer.bind(this);
+    }
+
+    async pick_answer(answer) {
+        const params = new URLSearchParams();
+
+        params.append('question_id', this.state.questions[this.state.current_question_index].QuestionID);
+        params.append('answer', answer);
+
+        const options = {}
+
+        if (localStorage.getItem('token') !== null) {
+            options['headers'] = {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+
+        const result = await axios.post(
+            'http://localhost:80/questions',
+            params,
+            options
+        )
+
+        console.log(result.data.stats[0]);
     }
 
     async componentDidMount() {
@@ -21,7 +50,42 @@ class VotePage extends React.Component {
         this.state.current_question_index = 0;
         this.state.questions = result.data.question;
 
-        console.log(this.state);
+        this.forceUpdate();
+    }
+
+    async get_question() {
+        let options = {};
+
+        if (localStorage.getItem('token') !== null) {
+            options['headers'] = {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        }
+
+        const result = await axios.get(
+            'http://localhost:80/questions',
+            options
+        )
+
+        this.state.questions.push(result.data.question[0]);
+    }
+
+    onPrevClick() {
+        if (this.state.current_question_index !== undefined &&
+            this.state.current_question_index > 0) {
+            this.state.current_question_index--;
+
+            this.forceUpdate();
+        }
+    }
+
+    async onNextClick() {
+        this.state.current_question_index++;
+
+        if (this.state.current_question_index === this.state.questions.length) {
+            await this.get_question();
+        }
+
         this.forceUpdate();
     }
 
@@ -29,8 +93,9 @@ class VotePage extends React.Component {
         return (
             <div className={'VotePage'}>
                 <div className={'ButtonsContainer'}>
-                    <button className={'NavigationButton'}>{'<'}</button>
-                    <button className={'ChoiceButton'} id={'blue_button'}>
+                    <button className={'NavigationButton'} onClick={this.onPrevClick}>{'<'}</button>
+                    <button className={'ChoiceButton'} id={'blue_button'}
+                            onClick={() => {this.pick_answer(1)}}>
                         {
                             'questions' in this.state ?
                                 this.state.questions[this.state.current_question_index].Answer1 :
@@ -38,14 +103,15 @@ class VotePage extends React.Component {
                         }
                     </button>
                     <div id={'or'}>or</div>
-                    <button className={'ChoiceButton'} id={'red_button'}>
+                    <button className={'ChoiceButton'} id={'red_button'}
+                            onClick={() => {this.pick_answer(2)}}>
                         {
                             'questions' in this.state ?
                                 this.state.questions[this.state.current_question_index].Answer2 :
                                 'Red'
                         }
                     </button>
-                    <button className={'NavigationButton'}>{'>'}</button>
+                    <button className={'NavigationButton'} onClick={this.onNextClick}>{'>'}</button>
                 </div>
             </div>
         )
