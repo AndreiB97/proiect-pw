2e34748a00863f09499f308871046891b7482773
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-24 14:18:39 +0300

    added admin / support page redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,9 @@ import Header from "./Header";
 import VotePage from "./VotePage";
 import FAQPage from "./FAQPage";
 import ContactPage from "./ContactPage";
-import axios from "axios";
+import AdminPage from "./AdminPage";
 
-// TODO admin account page
+// TODO support account page
 // TODO gdpr form
 // TODO remove useless class names
 
@@ -25,6 +25,14 @@ class App extends React.Component {
                     <Header refresh={this.forceUpdate}/>
                     <hr/>
                     <div className={'Content'}>
+                        {
+                            localStorage.getItem('role') === '1' ?
+                                <Redirect to={'/admin'}/> :
+                                localStorage.getItem('role') === '2' ?
+                                    <Redirect to={'/support'}/> :
+                                    <span/>
+                        }
+                        <Route path={'/admin'} component={AdminPage}/>
                         <Route exact path={'/'} component={() => {return (<VotePage/>)}}/>
                         <Route path={'/faq'} component={() => {return (<FAQPage/>)}}/>
                         <Route path={'/contact'} component={() => {return (<ContactPage/>)}}/>
