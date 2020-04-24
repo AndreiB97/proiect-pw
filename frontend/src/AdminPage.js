2dd13930bbae7fd58d3df53eb06dd33fb24ed6f7
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-24 14:19:25 +0300

    added admin page

---
+++ frontend/src/AdminPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AdminPage.scss'
+import AdminRegistration from "./AdminRegistration";
+
+class AdminPage extends React.Component {
+    render() {
+        return (
+            <AdminRegistration/>
+        );
+    }
+}
+
+export default AdminPage;
