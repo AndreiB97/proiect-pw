2d723b502e0e489b6b23f94c1ce5690529efcf9b
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-24 12:26:50 +0300

    refactoring: - changed logic so parent state is not changed anymore - fixed some display bugs - renamed or removed some css classes

--- frontend/src/ContactPage.js
+++ frontend/src/ContactPage.js
@@ -8,18 +8,17 @@ class ContactPage extends React.Component {
         super(props);
 
         this.onSend = this.onSend.bind(this);
-        this.onTextChange = this.onTextChange.bind(this);
 
-        this.state = {'text_value': ''};
-
-        this.handler = this.props.handler;
-    }
-
-    onTextChange(event) {
-        this.setState({'text_value': event.target.value});
+        this.state = {
+            'text_value': ''
+        };
     }
 
     async onSend() {
+        if (this.state.text_value.length === 0) {
+            return;
+        }
+
         let params = new URLSearchParams();
 
         params.append('message', this.state.text_value);
@@ -34,63 +33,96 @@ class ContactPage extends React.Component {
             }
         );
 
+        console.log(this.state.text_value);
+
         this.state.text_value = '';
 
         this.forceUpdate();
-
-        await this.props.get_messages();
     }
 
     render() {
-        if (localStorage.getItem('token') !== null) {
-            return (
-                <div className={'ContactPage'}>
-                    <h1>Message us below</h1>
-                    <form onSubmit={this.onSend}>
-                        <textarea className={'text-box'} value={this.state.text_value} autoFocus={true} maxLength={'512'}
-                                  onChange={this.onTextChange} placeholder={'Maximum 512 characters.'} required/>
-                        <input className={'send-button'} type={'submit'} value={'Send'}/>
-                    </form>
-                    <br/>
-                    <br/>
-                    <Conversations state={this.props.state} handler={this.props.handler}/>
-                </div>
-            )
-        } else {
-            return (
-                <div className={'ContactPage'}>
-                    <h1>You must be logged in to send messages.</h1>
-                </div>
-            )
-        }
+        return (
+            <div className={'ContactPage'}>
+                {
+                    localStorage.getItem('token') !== null ?
+                        <span>
+                            <h1>Message us below</h1>
+                            <form onSubmit={this.onSend}>
+                                <textarea className={'TextBox'} value={this.state.text_value} autoFocus={true}
+                                          maxLength={'512'} placeholder={'Maximum 512 characters.'}
+                                          onChange={(event) => {
+                                              this.setState({'text_value': event.target.value});
+                                          }}/>
+                                <br/>
+                                <input className={'SendButton'} type={'submit'} value={'Send'}/>
+                            </form>
+                            <br/>
+                            <br/>
+                            <Conversations/>
+                        </span> :
+                        <h1>You must be logged in to send messages.</h1>
+                }
+            </div>
+        );
     }
 }
 
 class Conversations extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            'with_response_messages': [],
+            'no_response_messages': []
+        };
+    }
+
+    async componentDidMount() {
+        const result = await axios.get(
+            'http://localhost:80/contact',
+            {
+                'headers': {
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                }
+            }
+        )
+
+        this.state.no_response_messages = result.data.no_response_messages;
+        this.state.with_response_messages = result.data.with_response_messages;
+
+        this.forceUpdate();
+    }
+
     render() {
         return (
-            <div className={'Conversations'}>
-                {this.props.state().with_response_messages ?
-                    <span>
-                        <h1>Answered messages:</h1>
-                        {this.props.state().with_response_messages.map((message) => {
-                            return <CollapsibleItem header={message.Message.substr(0, 32)}
-                                                    content={message.Message} response={message.Response}
-                                                    admin_name={message.Username} />
-                        })}
-                    </span> :
-                    <span></span>
+            <div>
+                {
+                    this.state.with_response_messages.length > 0 ?
+                        <span>
+                            <h1>Answered messages:</h1>
+                            {
+                                this.state.with_response_messages.map((message) => {
+                                    return <CollapsibleItem header={message.Message.substr(0, 32)}
+                                                            content={message.Message} response={message.Response}
+                                                            admin_name={message.Username} />
+                                })
+                            }
+                        </span> :
+                        <span/>
                 }
-                {this.props.state().no_response_messages ?
-                    <span>
-                        <h1>Not yet answered messages:</h1>
-                        {this.props.state().no_response_messages.map((message) => {
-                            return <CollapsibleItem header={message.Message.substr(0, 32)}
-                                                    content={message.Message} response={message.Response}
-                                                    admin_name={message.Username} />
-                        })}
-                    </span> :
-                    <span></span>
+                {
+                    this.state.no_response_messages.length > 0 ?
+                        <span>
+                            <h1>Not yet answered messages:</h1>
+                            {
+                                this.state.no_response_messages.map((message) => {
+                                    console.log('here');
+                                    return <CollapsibleItem header={message.Message.substr(0, 32)}
+                                                            content={message.Message}/>
+                                })
+                            }
+                        </span> :
+                        <span/>
                 }
             </div>
         )
