7de78711041f72e3a6278c2eed6660e85e39f871
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-24 12:24:39 +0300

    removed useless functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {HashRouter, Route} from 'react-router-dom'
+import {HashRouter, Redirect, Route} from 'react-router-dom'
 import Header from "./Header";
 import VotePage from "./VotePage";
 import FAQPage from "./FAQPage";
@@ -8,61 +8,26 @@ import ContactPage from "./ContactPage";
 import axios from "axios";
 
 // TODO admin account page
-// TODO admin should not be able to do regular user stuff so limit
 // TODO gdpr form
-// TODO rewrite certain parts of the code
-// TODO move get messages
+// TODO remove useless class names
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.handler = this.handler.bind(this);
-        this.get_state = this.get_state.bind(this);
-        this.get_messages = this.get_messages.bind(this);
-
-        this.state = {
-            username: 'none',
-            password: 'none'
-        };
-    }
-
-    handler(new_state) {
-        this.setState(new_state);
-    }
-
-    get_state() {
-        return this.state;
-    }
-
-    async get_messages() {
-        const result = await axios.get(
-            'http://localhost:80/contact',
-            {
-                'headers': {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                }
-            }
-        )
-
-        this.setState(result.data);
+        this.forceUpdate = this.forceUpdate.bind(this);
     }
 
     render() {
         return (
             <HashRouter>
                 <div className="App">
-                    <Header state={this.get_state} handler={(new_state) => {this.setState(new_state)}}
-                            get_messages={this.get_messages}/>
+                    <Header refresh={this.forceUpdate}/>
                     <hr/>
                     <div className={'Content'}>
-                        <Route exact path={'/'} component={() => {return (<VotePage state={this.get_state}
-                                                                                    handler={this.handler}/>)}}/>
-                        <Route path={'/faq'} component={() => {return (<FAQPage state={this.get_state}
-                                                                                handler={this.handler}/>)}}/>
-                        <Route path={'/contact'} component={() => {return (<ContactPage state={this.get_state}
-                                                                                        handler={this.handler}
-                                                                                        get_messages={this.get_messages}/>)}}/>
+                        <Route exact path={'/'} component={() => {return (<VotePage/>)}}/>
+                        <Route path={'/faq'} component={() => {return (<FAQPage/>)}}/>
+                        <Route path={'/contact'} component={() => {return (<ContactPage/>)}}/>
                     </div>
                 </div>
             </HashRouter>
