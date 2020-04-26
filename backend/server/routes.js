a595d254b064f7fc267d7715ad6317ee4d5ee824
AndreiB97 <andrei_birzan@yahoo.com>
2020-04-26 10:29:51 +0300

    added /support/messages route

--- backend/server/routes.js
+++ backend/server/routes.js
@@ -7,6 +7,8 @@ const users = require('./users.js');
 // TODO make sure admins can't do support actions
 // TODO recheck status codes
 // TODO maybe split this into multiple files
+// TODO check for token before any other checks
+
 const cors_header_name = 'Access-Control-Allow-Origin';
 const cors_header_value = '*';
 
@@ -25,9 +27,46 @@ router.get('/support/messages', (req, res) => {
         return;
     }
 
-    mysql.call_proc('get_unanswered_messages', [], (result) => {
+    mysql.call_proc('get_messages', [], (result) => {
         res.status(200).json(result).end();
     })
+});
+
+router.post('/support/messages', (req, res) => {
+    res.header(cors_header_name, cors_header_value);
+
+    if (! ('message_id' in req.body)) {
+        res.status(400).json({'error': 'Message ID missing'}).end();
+        return;
+    }
+
+    if (! req.headers.authorization) {
+        res.status(400).json({'error': 'Token missing'}).end();
+        return;
+    }
+
+    const user_data = users.log_admin(req.headers.authorization.split(' ')[1]);
+
+    if (user_data === undefined) {
+        res.status(400).json({'error': 'Invalid token'}).end();
+        return;
+    }
+
+    if ('response' in req.body) {
+        mysql.call_proc('respond_to_message', [req.body.message_id, user_data.AdminID,
+            req.body.response], () => {
+            res.status(200).end();
+        });
+    } else if ('important' in req.body) {
+        mysql.call_proc('flag_message_important', [req.body.message_id, req.body.important],
+            () => {
+            res.status(200).end();
+        });
+    } else {
+        mysql.call_proc('report_message_author', [req.body.message_id], () => {
+            res.status(200).end();
+        });
+    }
 });
 
 router.post('/admin/register', (req, res) => {
